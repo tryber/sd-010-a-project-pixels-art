ed64e7d021e76c00f0f4b3bdd36add5d7a52300e
rcalvs <rafaelcalvette@gmail.com>
2021-03-10 15:17:04 -0300

    verificaçao de acertos

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ window.onload = function bemVindo() {
 }
 
 
-//Variaveis //
+//Variaveis // pq Const? //
 
 let colorElements = document.querySelectorAll('.color');
 let board = document.getElementById('board');
+let palleteColorBasic = ['black', 'red', 'blue', 'green'];
 
 // Coloca as cores nas Divs //
-let palleteColorBasic = ['black', 'red', 'blue', 'green'];
 window.onload = function palleteColor(){
     for (let index = 0; index < palleteColorBasic.length; index += 1)
     colorElements[index].style.backgroundColor = palleteColorBasic[index];
@@ -21,33 +21,39 @@ window.onload = function palleteColor(){
 
 
 // Seleciona cor na palette //
-function selecColorPallete(event) {
-    const elementTarget = event.target;
+function selectColorPallete(event) {
+    let elementTarget = Event.target;
     if (elementTarget.id !== 'color-palette') {
       colorElements.forEach((element) => {
         element.classList.remove('selected');
-        elementTarget.classList.add('selected');
+        event.target.classList.add('selected');
       });
     }
   }
 
-console.log(board)
+// Salva cor //
+  function saveColorPalette() {
+    let colorSelected = document.querySelector('.selected');
+    let color = window.getComputedStyle(colorSelected).getPropertyValue('background-color');
+    return color;
+  }
+
+// Pinta pixel //
+function paintPixels(event) {
+    const elementTarget = event.target;
+    if (elementTarget.id !== 'board') {
+      elementTarget.style.backgroundColor = selectColorPallete();
+    }
+  }
 
 
+console.log(colorElements)
 
 
+colorElements[0].addEventListener('click', selectColorPallete);
+colorElements[1].addEventListener('click', selectColorPallete);
+colorElements[2].addEventListener('click', selectColorPallete);
+colorElements[3].addEventListener('click', selectColorPallete);
+board.addEventListener('click', paintPixels);
 
-// let pixel = document.getElementsByClassName('pixel');
-
-// pixel.onclick = changeColor 
-
-
-// function changeColor() {
-//     document.getElementsByClassName('pixel').style.backgroundColor = 'black'
-    
-// document.getElementsByTagName('td')
-
-// document.getElementsByTagName('td')[17].style.backgroundColor = palleteColorBasic[3]
-
-}
 
